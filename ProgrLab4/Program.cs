7f4906f85456f5784001d2046e7ee00a8730ad98
GreatSea1 <user.name>
2022-05-29 18:50:48 +0700

    Working Memento
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    Какой же шарп отвратительный боже

---
+++ ProgrLab4/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ProgrLab4
+{
+  public interface IOriginator
+  {
+    object GetMemento();
+    void SetMemento(object Memento);
+  }
+
+  class FileMemento
+  {
+    public string FirstName;
+    public string LastName;
+  }
+
+  [Serializable]
+  public class Patient : IOriginator
+  {
+    public string _FirstName { get; set; }
+    public string _LastName { get; set; }
+
+    public Patient(string FirstName, string LastName)
+    {
+      _FirstName = FirstName;
+      _LastName = LastName;
+    }
+
+    public object GetMemento()
+    {
+      return new FileMemento
+      {
+        FirstName = this._FirstName,
+        LastName = this._LastName,
+      };
+    }
+
+    public void SetMemento(object Memento)
+    {
+      if (Memento is FileMemento)
+      {
+        var Mem = Memento as FileMemento;
+        _FirstName = Mem.FirstName;
+        _LastName = Mem.LastName;
+      }
+    }
+  }
+
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Patient pt = new Patient("John", "Doen");
+
+      FileStream stream = File.Create(@"C:\Users\pawsw\source\repos\ProgLab4\ProgrLab4\Files\Patient.dat");
+      BinaryFormatter formatter = new BinaryFormatter();
+
+      //Сериализация
+      formatter.Serialize(stream, pt);
+      stream.Close();
+
+      //Десериализация
+      stream = File.OpenRead(@"C:\Users\pawsw\source\repos\ProgLab4\ProgrLab4\Files\Patient.dat");
+
+      pt = formatter.Deserialize(stream) as Patient;
+
+      Console.WriteLine("Имя    : " + pt._FirstName);
+      Console.WriteLine("Фамилия: " + pt._LastName);
+      stream.Close();
+      Console.ReadKey();
+
+      //Поиск файла
+      SearchingFiles Search = new();
+      Search.Search();
+
+      Console.Write("Введите текст: ");
+      string Searchable = Console.ReadLine();
+
+      Patient FirstFile = new Patient("Matt", "Smith");
+      Patient SecondFile = new Patient("Peter", "Capaldi");
+      Patient ThirdFile = new Patient("Jodie", "Whittaker");
+
+      List<Patient> ListOfFiles = new List<Patient>
+            {
+                FirstFile, SecondFile, ThirdFile
+            };
+
+      foreach (var File in ListOfFiles)
+      {
+        if (File._FirstName.Contains(Searchable))
+        {
+          Console.WriteLine($"{Searchable} in {File._LastName}");
+        }
+        else
+        {
+          Console.WriteLine($"{File._LastName} не содержит {Searchable}");
+        }
+      }
+    }
+  }
+}
---
+++ ProgrLab4/SearchingFiles.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrLab4
+{
+  public class SearchingFiles
+  {
+    string FileName;
+    string CatalogFromSearch = @"C:\Users\pawsw\source\repos\ProgLab4\ProgrLab4\Files";
+    public void Search()
+    {
+      Console.WriteLine("Введите название файла: ");
+      FileName = Console.ReadLine();
+      FileName = FileName + ".txt";
+      foreach (string FoundedFile in Directory.EnumerateFiles(CatalogFromSearch, FileName, SearchOption.AllDirectories))
+      {
+        FileInfo FI;
+        try
+        {
+          //Создаётся объект класса, который содержит в себе ссылку на найденный файл
+          FI = new FileInfo(FoundedFile);
+          Console.WriteLine(FI.Name + " " + FI.FullName + " " + FI.Length + " байт " + " Создан " + FI.CreationTime);
+        }
+        catch
+        {
+          continue;
+        }
+      }
+    }
+  }
+}
